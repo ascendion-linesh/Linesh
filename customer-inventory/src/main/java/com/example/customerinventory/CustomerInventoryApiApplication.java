87a6d68e7a2358331c096d87eab1bd4058ff5803
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-23 14:13:11 +0530

    Add or update file: CustomerInventoryApiApplication.java

---
+++ customer-inventory/src/main/java/com/example/customerinventory/CustomerInventoryApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.customerinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerInventoryApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerInventoryApiApplication.class, args);
+    }
+}
