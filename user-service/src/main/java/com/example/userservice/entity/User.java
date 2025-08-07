9d4601f441fd88282d855bbb6bb73a17bddf7ba3
ascendion-linesh <linesh.mishra@ascendion.com>
2025-08-07 11:48:59 +0530

    Add or update file: User.java

---
+++ user-service/src/main/java/com/example/userservice/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private int totalOrders;
+
+    @Column(nullable = false)
+    private double totalSpent;
+}
